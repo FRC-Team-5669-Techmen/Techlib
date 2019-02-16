5b51f4a0d547b06b81252d915ad7ae6d1f563d08
IDEA Computer Lab <idea_lab@boscotech.edu>
2019-02-15 21:59:42 -0800

    Added buttons to Controls class.

--- config/Controls.java
+++ config/Controls.java
@@ -4,10 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class Controls {
   private static Controls s_instance;
   private Map<String, AnalogControl> analogControls = new HashMap<>();
+  private Map<String, JoystickButton> digitalControls = new HashMap<>();
 
   private Controls() { }
 
@@ -44,5 +46,29 @@ public class Controls {
     }
 
     return analogControls.get(catPath);
+  }
+  
+  public JoystickButton getDigitalControl(String... path) {
+    String catPath = "";
+    for (String part : path) {
+      catPath += part;
+      catPath += ":";
+    }
+
+    if (digitalControls.get(catPath) == null) {
+      // Make a new String array with one extra element on either end.
+      String[] realpath = new String[path.length + 2];
+      System.arraycopy(path, 0, realpath, 1, path.length);
+      realpath[0] = "controls";
+
+      Config cfg = Config.getInstance();
+      realpath[path.length + 1] = "joystick";
+      Joystick stick = new Joystick(cfg.getInt(realpath));
+      realpath[path.length + 1] = "button";
+      int button = cfg.getInt(realpath);
+      digitalControls.put(catPath, new JoystickButton(stick, button));
+    }
+
+    return digitalControls.get(catPath);
   }
 }
