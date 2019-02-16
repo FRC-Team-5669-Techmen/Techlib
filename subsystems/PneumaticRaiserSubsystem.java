1792458d2264ec092fde5fed1f7a73e3cdc17ed1
IDEA Computer Lab <idea_lab@boscotech.edu>
2019-02-16 09:10:13 -0800

    Converted raiser subsystem.

--- subsystems/PneumaticRaiserSubsystem.java
+++ subsystems/PneumaticRaiserSubsystem.java
@@ -1,66 +1,35 @@
 package edu.boscotech.techlib.subsystems;
 
 import edu.boscotech.techlib.commands.TestPneumaticRaiser;
-import edu.boscotech.techlib.config.Config;
-import edu.boscotech.techlib.util.DefaultCommandCreator;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SendableBuilderImpl;
 
-public class PneumaticRaiserSubsystem extends Subsystem
-  implements DefaultCommandCreator {
-  private static Config cfg = Config.getInstance();
-  private SendableBuilderImpl networkData = new SendableBuilderImpl();
-  private boolean networkTablesControlOn = false;
-  private DoubleSolenoid frontSolenoid = new DoubleSolenoid(
-    cfg.getInt("subsystems", "raiser", "extendFront"),
-    cfg.getInt("subsystems", "raiser", "retractFront")
-  ), backSolenoid = new DoubleSolenoid(
-    cfg.getInt("subsystems", "raiser", "extendBack"),
-    cfg.getInt("subsystems", "raiser", "retractBack")
-  );
+public class PneumaticRaiserSubsystem extends BetterSubsystem {
+  private DoubleSolenoid frontSolenoid, backSolenoid;
 
   public PneumaticRaiserSubsystem() {
-    networkData.setTable(NetworkTableInstance.getDefault()
-      .getTable("Subsystems").getSubTable("PneumaticRaiser"));
-    networkData.setSmartDashboardType("PneumaticRaiser");
-    networkData.setActuator(true);
-    networkData.setSafeState(this::retractAllCylinders);
-    networkData.addBooleanProperty("Front Cylinders", 
-      this::isFrontCylindersExtended, this::setFrontCylindersExtended);
-    networkData.addBooleanProperty("Back Cylinders", 
-      this::isBackCylindersExtended, this::setBackCylindersExtended);
+    super("Pneumatic Raiser", "raiser", "PneumaticRaiser");
   }
 
-  /**
-   * Allows this subsystem to be driven manually via the NetworkTables api,
-   * which can be accessed from most dashboard systems (usually Shuffleboard).
-   * If control is already enabled, this method will exit early, making it safe
-   * to call frequently.
-   */
-  public void startNetworkTablesControl() {
-    if (networkTablesControlOn) return;
-    retractAllCylinders();
-    networkData.startListeners();
-    networkData.updateTable();
-    networkTablesControlOn = true;
+  public PneumaticRaiserSubsystem(String hrName, String cfgName) {
+    super(hrName, cfgName, "PneumaticRaiser");
   }
 
-  /**
-   * Stops this subsystem from being driven manually via the NetworkTables api,
-   * which is accessed by most dashboard systems (usually Shuffleboard). If
-   * control is already disabled, this method will exit early, making it safe
-   * to call frequently.
-   */
-  public void stopNetworkTablesControl() {
-    if (!networkTablesControlOn) return;
-    retractAllCylinders();
-    networkData.stopListeners();
-    networkData.updateTable();
-    networkTablesControlOn = false;
+  @Override
+  protected void setup() {
+    frontSolenoid = new DoubleSolenoid(
+      getCfgInt("extendFront"), getCfgInt("retractFront")
+    );
+    getNetworkData().addBooleanProperty("Front Cylinders", 
+      this::isFrontCylindersExtended, this::setFrontCylindersExtended
+    );
+    backSolenoid = new DoubleSolenoid(
+      getCfgInt("extendBack"), getCfgInt("retractBack")
+    );
+    getNetworkData().addBooleanProperty("Back Cylinders", 
+      this::isBackCylindersExtended, this::setBackCylindersExtended
+    );
   }
 
   public void extendFrontCylinders() {
@@ -106,11 +75,8 @@ public class PneumaticRaiserSubsystem extends Subsystem
   }
 
   @Override
-  public void initDefaultCommand() { }
-
-  @Override
-  public Command createDefaultTeleopCommand() {
-    return null;
+  protected void enterSafeState() {
+    retractAllCylinders();
   }
 
   @Override
