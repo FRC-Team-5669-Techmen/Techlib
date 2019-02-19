2433de558533167f0e501248fa960b9320b8a046
Code Cube <therealcodecube@gmail.com>
2019-02-19 15:23:51 -0800

    Ported lif subsystem to new encoded motor class.

--- subsystems/LiftSubsystem.java
+++ subsystems/LiftSubsystem.java
@@ -1,17 +1,10 @@
 package edu.boscotech.techlib.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
+import edu.boscotech.techlib.commands.DriveEncodedMotor;
 import edu.boscotech.techlib.commands.GenericTestCommand;
-import edu.boscotech.techlib.commands.SetLiftSpeed;
 import edu.wpi.first.wpilibj.command.Command;
 
-public class LiftSubsystem extends BetterSubsystem {
-  private TalonSRX m_motor;
-  private int m_minValue, m_maxValue, m_tolerance;
-  private double m_sensorUnitsPerInch;
-
+public class LiftSubsystem extends EncodedMotorSubsystem {
   public LiftSubsystem() {
     super("Lift", "lift", "Lift");
   }
@@ -22,64 +15,31 @@ public class LiftSubsystem extends BetterSubsystem {
 
   @Override
   protected void setup() {
-    getNetworkData().addDoubleProperty("Height", this::getPosition, 
-      this::goToPosition);
-    m_motor = new TalonSRX(getCfgInt("motor1"));
-    m_motor.setSensorPhase(true); // Sensor is backwards.
-    m_motor.setInverted(true); // Motor is backwards.
-    m_motor.configOpenloopRamp(0.5);
-    // TODO: Config stuff for motion profile.
+    setupMotor(getCfgInt("motor1"), "motionProfile", true, true);
   }
 
   public void goCompletelyDown() {
-    m_motor.set(ControlMode.MotionMagic, m_minValue);
+    setPosition(0.0);
   }
 
   public boolean isCompletelyDown() {
-    return m_motor.getSelectedSensorPosition() <= m_minValue + m_tolerance;
+    return getPosition() < 0.01;
   }
 
   public void goCompletelyUp() {
-    m_motor.set(ControlMode.MotionMagic, m_maxValue);
+    setPosition(1.0);
   }
 
   public boolean isCompletelyUp() {
-    return m_motor.getSelectedSensorPosition() >= m_maxValue - m_tolerance;
-  }
-
-  public void goToPosition(double inches) {
-    m_motor.set(ControlMode.MotionMagic, inches * m_sensorUnitsPerInch);
-  }
-
-  public double getPosition() {
-    return m_motor.getSelectedSensorPosition() / m_sensorUnitsPerInch;
-  }
-
-  public boolean isAtPosition(double inches) {
-    return Math.abs(
-      m_motor.getSelectedSensorPosition() - inches * m_sensorUnitsPerInch
-    ) < m_tolerance;
-  }
-
-  public void setManualSpeed(double speed) {
-    m_motor.set(ControlMode.PercentOutput, speed);
-  }
-
-  public void goNeutral() {
-    m_motor.neutralOutput();
-  }
-
-  @Override
-  protected void enterSafeState() {
-    goNeutral();
+    return getPosition() > 0.99;
   }
 
   @Override
   public Command createDefaultTeleopCommand() {
-    getDigitalControl("up").whenPressed(new SetLiftSpeed(this, 0.8));
-    getDigitalControl("up").whenReleased(new SetLiftSpeed(this, 0.0));
-    getDigitalControl("down").whenPressed(new SetLiftSpeed(this, -0.8));
-    getDigitalControl("down").whenReleased(new SetLiftSpeed(this, 0.0));
+    getDigitalControl("up").whenPressed(new DriveEncodedMotor(this, 0.8));
+    getDigitalControl("up").whenReleased(new DriveEncodedMotor(this, 0.0));
+    getDigitalControl("down").whenPressed(new DriveEncodedMotor(this, -0.8));
+    getDigitalControl("down").whenReleased(new DriveEncodedMotor(this, 0.0));
     return null;
   }
 
