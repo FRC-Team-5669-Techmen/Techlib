02ee9c3d3b98c8f80d86d21b92128f1108bd1e53
Code Cube <therealcodecube@gmail.com>
2019-04-17 08:46:58 -0700

    Added utility functions and refactored ConfigElement

--- configparser/ConfigElement.java
+++ configparser/ConfigElement.java
@@ -4,55 +4,101 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConfigElement {
-    private String name, type, value;
-    private List<ConfigElement> subValues = new ArrayList<>();
+    private String m_name, m_type, m_value;
+    private List<ConfigElement> m_children = new ArrayList<>();
 
     public ConfigElement(String name, String type, String value) {
-        this.name = name;
-        this.type = type;
-        this.value = value;
+        m_name = name;
+        m_type = type;
+        m_value = value;
     }
 
     /**
      * @return the name
      */
     public String getName() {
-        return name;
+        return m_name;
     }
 
     /**
      * @return the type
      */
     public String getType() {
-        return type;
+        return m_type;
     }
 
     /**
      * @return the value
      */
     public String getValue() {
-        return value;
+        return m_value;
     }
 
     public void addChild(ConfigElement child) {
-        subValues.add(child);
+        m_children.add(child);
     }
 
     public List<ConfigElement> getChildren() {
-        return subValues;
+        return m_children;
     }
 
-    public void print() {
-        print(0);
+    public ConfigElement getChild(String name) {
+        for (ConfigElement child : m_children) {
+            if (child.getName() == name) {
+                return child;
+            }
+        }
+        return null;
     }
 
-    public void print(int indentation) {
+    public ConfigElement getPath(String... path) {
+        ConfigElement current = this;
+        for (String name : path) {
+            current = current.getChild(name);
+            if (current == null) return null;
+        }
+        return current;
+    }
+
+    public String getString(String... path) {
+        return getPath(path).asString();
+    }
+
+    public double getNumber(String... path) {
+        return getPath(path).asNumber();
+    }
+
+    public String getStringOrDefault(String defaultValue, String... path) {
+        ConfigElement element = getPath(path);
+        return element == null ? defaultValue : element.asString();
+    }
+
+    public double getNumberOrDefault(double defaultValue, String... path) {
+        ConfigElement element = getPath(path);
+        return element == null ? defaultValue : element.asNumber();
+    }
+
+    public String asString() {
+        return m_value;
+    }
+
+    public double asNumber() {
+        return Double.parseDouble(m_value);
+    }
+
+    public String toString() {
+        return toStringInternal(0);
+    }
+
+    private String toStringInternal(int indentation) {
+        String result = "";
         for (int i = 0; i < indentation; i++) {
-            System.out.print('\t');
+            result += '\t';
         }
-        System.out.println("name(" + name + ") type(" + type + ") value(" + value + ")");
-        for (ConfigElement subValue : subValues) {
-            subValue.print(indentation + 1);
+        result += "name(" + m_name + ") type(" + m_type + ") value(" + m_value + ")\n";
+        for (ConfigElement child : m_children) {
+            result += child.toStringInternal(indentation + 1);
         }
+        return result;
     }
 }
