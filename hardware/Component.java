b817e634631f97df7edbc2cb72763d1137e78461
Code Cube <therealcodecube@gmail.com>
2019-04-24 09:26:29 -0700

    Elaborated Component class with an extra method and documentation.

--- hardware/Component.java
+++ hardware/Component.java
@@ -14,20 +14,49 @@ public abstract class Component implements Sendable {
         LiveWindow.add(this);
     }
 
+    /**
+     * Called when the component is first set up. Use this to initialize any
+     * member variables using values in configTree.
+     * @param configTree The configuration for this component, read from the
+     *                   robot's configuration file.
+     */
     protected abstract void setup(ConfigElement configTree);
+
+    /**
+     * Called regularly while the component is active. Use this to implement any
+     * logic that needs to be run to keep the component operating properly. For
+     * example, this could be used to set the output of a PWM port depending on
+     * a value set by a different method.
+     */
     protected abstract void periodic();
-    public abstract void enterSafeState();
+
+    /**
+     * This will put the component in a safe state such that it will not move
+     * suddenly or endanger any people around the robot. For example, if this
+     * component is a motor, this method will stop the motor.
+     */
+    protected abstract void enterSafeState();
+
+    /**
+     * Called when the network table manager is ready to start sending data.
+     * Override this method to add getters and setters so that information about
+     * this component can be displayed on the smart dashboard.
+     */
+    protected abstract void addNetworkData(SendableBuilder builder);
 
     public final void setupWrapper(ConfigElement configTree) {
         m_name = configTree.getStringOrDefault(
             configTree.getName(), "name"
         );
-        m_networkData.setSmartDashboardType(m_smartDashboardTypeName);
         setup(configTree);
     }
 
     public final void periodicWrapper() {
         periodic();
+    }
+
+    public final void enterSafeStateWrapper() {
+        enterSafeState();
     }
 
     @Override
@@ -55,5 +84,7 @@ public abstract class Component implements Sendable {
     @Override
     public final void initSendable(SendableBuilder builder) {
         m_networkData = builder;
+        m_networkData.setSmartDashboardType(m_smartDashboardTypeName);
+        addNetworkData(m_networkData);
     }
 }
