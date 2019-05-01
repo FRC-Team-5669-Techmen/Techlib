b96e987d027896798a63352c39db337802e0dc2c
Code Cube <therealcodecube@gmail.com>
2019-04-30 17:13:15 -0700

    Created annotations for configgable types.

---
+++ configparser/RealConfiggable.java
@@ -0,0 +1,9 @@
+package edu.boscotech.techlib.configparser;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME) 
+public @interface RealConfiggable {
+    String value();
+}
