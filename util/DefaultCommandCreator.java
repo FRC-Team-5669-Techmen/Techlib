7bee33982449a00fa80127650f9f02dc06404e0d
Code Cube <therealcodecube@gmail.com>
2019-05-01 17:22:19 -0700

    Deleted old code.

--- util/DefaultCommandCreator.java
+++
@@ -1,8 +0,0 @@
-package edu.boscotech.techlib.util;
-
-import edu.wpi.first.wpilibj.command.Command;
-
-public interface DefaultCommandCreator {
-  public Command createDefaultTeleopCommand();
-  public Command createDefaultTestCommand();
-}
